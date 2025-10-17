f7fdebebeda19321aa51d9566928119151ca6906
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-10-17 03:33:59 +0200

    whitespace

--- src/Core/Infrastructure/WebSocketServer.ts
+++ src/Core/Infrastructure/WebSocketServer.ts
@@ -64,62 +64,62 @@ export class WebSocketServer implements WebSocketServerInterface {
         }, this.heartBeatInterval)
 
         websocket.on('message', (message: WebSocket.RawData) => {
-            let data: object
+          let data: object
 
-            let messageString: string
-            if (Buffer.isBuffer(message)) {
-              messageString = message.toString('utf8')
-            } else if (message instanceof ArrayBuffer) {
-              messageString = new TextDecoder().decode(message)
-            } else {
-              messageString = message.toString()
-            }
+          let messageString: string
+          if (Buffer.isBuffer(message)) {
+            messageString = message.toString('utf8')
+          } else if (message instanceof ArrayBuffer) {
+            messageString = new TextDecoder().decode(message)
+          } else {
+            messageString = message.toString()
+          }
 
-            try {
-              data = JSON.parse(messageString) as object
-            } catch {
-              this.logger.info(`Received invalid JSON: ${messageString}`)
-              return
-            }
-
-            if (
-              'type' in data &&
-              data.type === 'chat_message' &&
-              'message' in data &&
-              typeof data.message === 'string'
-            ) {
-              this.handleChatMessage(data.message)
-
-              return
-            }
-
-            if (
-              'type' in data &&
-              'token' in data &&
-              typeof data.token === 'string'
-            ) {
-              const isValid = this.isTokenValid(data.token)
-
-              if (!isValid) {
-                this.logger.info('Authentication failed. Closing connection.')
-                websocket.send('auth_failed')
-                websocket.close()
-                return
-              }
-
-              this.authenticatedClients.add(websocket)
-
-              this.logger.info(
-                `New client authenticated. Number of authenticated clients: ${this.authenticatedClients.size.toString()}`,
-              )
-
-              clearTimeout(authTimeout)
-
-              return
-            }
-
-            websocket.close()
+          try {
+            data = JSON.parse(messageString) as object
+          } catch {
+            this.logger.info(`Received invalid JSON: ${messageString}`)
             return
+          }
+
+          if (
+            'type' in data &&
+            data.type === 'chat_message' &&
+            'message' in data &&
+            typeof data.message === 'string'
+          ) {
+            this.handleChatMessage(data.message)
+
+            return
+          }
+
+          if (
+            'type' in data &&
+            'token' in data &&
+            typeof data.token === 'string'
+          ) {
+            const isValid = this.isTokenValid(data.token)
+
+            if (!isValid) {
+              this.logger.info('Authentication failed. Closing connection.')
+              websocket.send('auth_failed')
+              websocket.close()
+              return
+            }
+
+            this.authenticatedClients.add(websocket)
+
+            this.logger.info(
+              `New client authenticated. Number of authenticated clients: ${this.authenticatedClients.size.toString()}`,
+            )
+
+            clearTimeout(authTimeout)
+
+            return
+          }
+
+          websocket.close()
+          return
         })
 
         websocket.on('close', () => {
