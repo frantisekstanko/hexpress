6d5c79aeb5b160168835b9e0416b46e0b989fe97
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-10-18 13:46:50 +0200

    refactor: prefer early returns

--- src/Core/Infrastructure/WebSocket/WebSocketMessageParser.ts
+++ src/Core/Infrastructure/WebSocket/WebSocketMessageParser.ts
@@ -5,15 +5,20 @@ import { WebSocketMessageParserInterface } from '@/Core/Application/WebSocket/We
 @injectable()
 export class WebSocketMessageParser implements WebSocketMessageParserInterface {
   public parseMessage(message: WebSocket.RawData): object {
-    let messageString: string
-    if (Buffer.isBuffer(message)) {
-      messageString = message.toString('utf8')
-    } else if (message instanceof ArrayBuffer) {
-      messageString = new TextDecoder().decode(message)
-    } else {
-      messageString = message.toString()
-    }
+    const messageString = this.webSocketMessageToString(message)
 
     return JSON.parse(messageString) as object
+  }
+
+  private webSocketMessageToString(message: WebSocket.RawData): string {
+    if (Buffer.isBuffer(message)) {
+      return message.toString('utf8')
+    }
+
+    if (message instanceof ArrayBuffer) {
+      return new TextDecoder().decode(message)
+    }
+
+    return message.toString()
   }
 }
