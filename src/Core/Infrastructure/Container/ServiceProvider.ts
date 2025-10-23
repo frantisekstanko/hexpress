08319a393be1589c022d88cecc862c3709ff556f
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-10-23 02:13:08 +0200

    flatten service provider hierarchy
    each provider now has single responsibility of registering its own
    services without delegating to other providers

--- src/Core/Infrastructure/Container/ServiceProvider.ts
+++ src/Core/Infrastructure/Container/ServiceProvider.ts
@@ -3,11 +3,6 @@ import { ServiceProviderInterface } from '@/Core/Application/ServiceProviderInte
 import { Services } from '@/Core/Application/Services'
 import { ApplicationVersionRepository } from '@/Core/Infrastructure/ApplicationVersionRepository'
 import { Config } from '@/Core/Infrastructure/Config'
-import { CommandServiceProvider } from '@/Core/Infrastructure/Container/CommandServiceProvider'
-import { DatabaseServiceProvider } from '@/Core/Infrastructure/Container/DatabaseServiceProvider'
-import { EventServiceProvider } from '@/Core/Infrastructure/Container/EventServiceProvider'
-import { HttpServiceProvider } from '@/Core/Infrastructure/Container/HttpServiceProvider'
-import { WebSocketServiceProvider } from '@/Core/Infrastructure/Container/WebSocketServiceProvider'
 import { Filesystem } from '@/Core/Infrastructure/Filesystem/Filesystem'
 import { LifecycleManager } from '@/Core/Infrastructure/LifecycleManager'
 import { Logger } from '@/Core/Infrastructure/Logger'
@@ -39,12 +34,6 @@ export class ServiceProvider implements ServiceProviderInterface {
       Services.UuidRepositoryInterface,
       () => new UuidRepository(),
     )
-
-    new DatabaseServiceProvider().register(container)
-    new CommandServiceProvider().register(container)
-    new EventServiceProvider().register(container)
-    new WebSocketServiceProvider().register(container)
-    new HttpServiceProvider().register(container)
 
     container.register(
       Services.LifecycleManagerInterface,
